fea7dbb5f6dbd2956adf39e3f2fe1114612ca581
Ricardo Manoel <rdomanoel@gmail.com>
2021-03-21 23:11:06 -0300

    dev: apollo client for github api connection

---
+++ src/graphql/client.ts
@@ -0,0 +1,9 @@
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+
+export const client = new ApolloClient({
+  uri: "https://api.github.com/graphql",
+  headers: {
+    Authorization: `Bearer 365f1c1ae938912fd77a34cc4dad37fbcde762cf`,
+  },
+  cache: new InMemoryCache(),
+});
