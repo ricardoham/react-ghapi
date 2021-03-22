28246fdf5d57c1c61d700c332c47328eea33d42e
Ricardo Manoel <rdomanoel@gmail.com>
2021-03-22 00:19:55 -0300

    dev: pagination functions

--- src/pages/Home/App.tsx
+++ src/pages/Home/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useQuery } from '@apollo/client';
 import { REPOSITORY } from '../../graphql/query';
+import { Variables } from 'model/variables';
 import Button from 'components/Button';
 import List from 'components/List';
 import Loader from 'components/Loader';
@@ -8,7 +9,7 @@ import Loader from 'components/Loader';
 import { Container } from './styles';
 
 function App() {
-  const { loading, error, data } = useQuery(REPOSITORY, {
+  const { loading, error, data, fetchMore } = useQuery(REPOSITORY, {
     variables: {
       queryString: 'react sort:stars',
       after: null,
@@ -18,13 +19,50 @@ function App() {
     },
   });
 
+  const handlePage = (variables: Variables) => {
+    fetchMore({
+      variables: {
+        after: variables.after,
+        first: variables.first,
+        before: variables.before,
+        last: variables.last,
+      },
+
+      updateQuery: (previousResult: any, { fetchMoreResult }: any) => {
+        const newNodes = fetchMoreResult.search.nodes;
+
+        return newNodes.length ? fetchMoreResult : previousResult;
+      },
+    });
+  };
+
   if (error) return <div>Error {error.message} </div>;
 
   return (
     <Container>
       {loading ? <Loader /> : <List data={data.search.nodes} />}
-      <Button text="Previous" onClick={() => console.log('CLICKED!')} />
-      <Button text="Next" onClick={() => console.log('CLICKED!')} />
+      <Button
+        text="Previous"
+        onClick={() =>
+          handlePage({
+            first: null,
+            after: null,
+            last: 10,
+            before: data.search.pageInfo.startCursor || null,
+          })
+        }
+      />
+      <Button
+        text="Next"
+        onClick={() =>
+          handlePage({
+            first: 10,
+            after: data.search.pageInfo.endCursor || null,
+            last: null,
+            before: null,
+          })
+        }
+      />
     </Container>
   );
 }
